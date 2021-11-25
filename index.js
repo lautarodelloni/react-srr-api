2e823e567837eb698b925fa077fc3522b7b337f9
Lautaro Delloni <lautaro.delloni@mercadolibre.com>
2021-11-25 14:05:31 -0300

    fix

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.get('/', (req, res) => {
 app.use('/api/users' ,usersRoute);
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${PORT}`)
 })
